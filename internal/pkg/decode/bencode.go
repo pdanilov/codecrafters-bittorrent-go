c50312422f3d5982cde0bf36416a4f22075b0e63
Pavel Danilov <pdanilov94@gmail.com>
2024-10-03 20:26:25 +0600

    Decode bencoded dictionaries

--- internal/pkg/decode/bencode.go
+++ internal/pkg/decode/bencode.go
@@ -14,6 +14,9 @@ import (
 // - le -> []
 // - l9:pineapplei254ee -> ["pineapple",254]
 // - lli4eei5ee -> [[4],5]
+// - de -> {}
+// - d3:foo6:banana5:helloi52ee -> {"foo":"banana","hello":52}
+// - d5:innerd4:key16:value14:key2i42e4:listl5:item15:item2i3eeee -> {"inner":{"key1":"value1","key2":42,"list":["item1","item2",3]}}
 func DecodeBencode(bencodedString string) (any, error) {
 	r := strings.NewReader(bencodedString)
 	return bencode.Decode(r)
