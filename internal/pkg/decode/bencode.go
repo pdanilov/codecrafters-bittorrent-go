7abc6d02e4cd198dbd334483c0a9fff081325c80
Pavel Danilov <pdanilov94@gmail.com>
2024-10-03 20:20:48 +0600

    Decode bencoded lists

--- internal/pkg/decode/bencode.go
+++ internal/pkg/decode/bencode.go
@@ -11,6 +11,9 @@ import (
 // - 10:hello12345 -> hello12345
 // - i1912808033e -> 1912808033
 // - i-52e -> -52
+// - le -> []
+// - l9:pineapplei254ee -> ["pineapple",254]
+// - lli4eei5ee -> [[4],5]
 func DecodeBencode(bencodedString string) (any, error) {
 	r := strings.NewReader(bencodedString)
 	return bencode.Decode(r)
