90f67cd9ae1b39514c40f5e24567aa425f2fada0
Pavel Danilov <pdanilov94@gmail.com>
2024-10-03 20:18:48 +0600

    Decode bencoded integers

--- internal/pkg/decode/bencode.go
+++ internal/pkg/decode/bencode.go
@@ -9,6 +9,8 @@ import (
 // Example:
 // - 5:hello -> hello
 // - 10:hello12345 -> hello12345
+// - i1912808033e -> 1912808033
+// - i-52e -> -52
 func DecodeBencode(bencodedString string) (any, error) {
 	r := strings.NewReader(bencodedString)
 	return bencode.Decode(r)
